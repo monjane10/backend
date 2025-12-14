d7504ea7a7b9bdb602049dbf45d392e3141cbdd7
monjane10 <louramonja52.n@gmail.com>
2025-12-14 15:11:45 +0200

    correção de bugs

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,31 @@
-import { buildApp } from './app';
+/**
+ * Autor: Lourenço Monjane
+ */
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import dotenv from "dotenv";
+import {routes} from "./routes"
 
-const app = buildApp();
+
+const app = Fastify({logger: true});
+dotenv.config();
+
+app.setErrorHandler((error, request, reply) => {
+    reply.code(400).send({message: error.message});
+    
+});
 
 const start = async () => {
-  try {
-    await app.listen({ port: 3333, host: '0.0.0.0' });
-    console.log('Server is running on http://localhost:3333');
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-};
+    app.register(cors);
+    app.register(routes);
+
+    try{
+        await app.listen({port: 3333, host: '0.0.0.0'});
+        console.log('Server is running on http://localhost:3333');
+    } catch(err){
+       console.log(err); 
+    }
+}
+
 
 start();
